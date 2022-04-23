0f015ce4a16820fbb60b1f81e9727359662a3429
philipuuu <topgun0415@naver.com>
2022-04-23 23:52:38 +0900

    ✨Feat : React Router Pratice

---
+++ src/pages/Diary.js
@@ -0,0 +1,12 @@
+/** @format */
+
+const Diary = () => {
+  return (
+    <div>
+      <h1>Diary</h1>
+      <p>이곳은 일기 상세페이지입니다</p>
+    </div>
+  );
+};
+
+export default Diary;
---
+++ src/pages/New.js
@@ -0,0 +1,12 @@
+/** @format */
+
+const New = () => {
+  return (
+    <div>
+      <h1>New</h1>
+      <p>이곳은 추가 작성페이지입니다</p>
+    </div>
+  );
+};
+
+export default New;
